2f7a5924e2137cd52b4a327371c3b982979e25f6
XC <vcaiza@tecnologicoismac.edu.ec>
2025-05-21 11:25:38 -0500

    fdsafds

---
+++ src/test/java/com/distribuida/test/FacturaTest.java
@@ -0,0 +1,15 @@
+package com.distribuida.test;
+
+import org.distribuida.entities.Factura;
+import org.distribuida.entities.cliente;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FacturaTest {
+
+}
+
+
